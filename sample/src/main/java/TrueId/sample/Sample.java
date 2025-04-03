da8b2e62d42804a1bd0d5b62e0ae04b965dfb162
saishankarsingamsetty <saishankar.s@trueid.in>
2025-04-03 10:11:19 +0530

    aa

--- sample/src/main/java/TrueId/sample/Sample.java
+++ sample/src/main/java/TrueId/sample/Sample.java
@@ -5,6 +5,7 @@ package TrueId.sample;
 
 public class Sample {
 	public static void main(String[] args) {
-//		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println();
 	}
 }
